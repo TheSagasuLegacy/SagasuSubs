c70b8b6ad3963a39b4f4e96cf786759f03e37f64
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-22 00:25:48 +0800

    :bug: Add `AdvancedSemaphore` to fix unclosed client warning

--- SagasuSubs/utils.py
+++ SagasuSubs/utils.py
@@ -1,9 +1,29 @@
 import hashlib
 import os
+from asyncio import Future, Semaphore
 from pathlib import Path
 from typing import Iterator, Optional
 
 import cchardet
+
+
+class AdvanceSemaphore(Semaphore):
+    def __init__(self, value: int) -> None:
+        super().__init__(value=value)
+        self._initial_value = value
+        self._finshed_future: Future[None] = Future()
+
+    def _check_value(self) -> None:
+        if (self._value >= self._initial_value) and (not self._waiters):
+            self._finshed_future.set_result(None)
+
+    def release(self) -> None:
+        super().release()
+        self._check_value()
+
+    async def wait_all_finish(self):
+        self._check_value()
+        await self._finshed_future
 
 
 def sha1sum(file: Path) -> str:
