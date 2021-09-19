30b69d8c78131f1fb34af58f00f01052a0c635a0
Mix <32300164+mnixry@users.noreply.github.com>
2021-09-19 23:30:27 +0800

    :bug: Fix `AdvanceSemaphore` future already done error

--- SagasuSubs/utils.py
+++ SagasuSubs/utils.py
@@ -1,6 +1,6 @@
 import hashlib
 import os
-from asyncio import AbstractEventLoop, Semaphore, wait_for
+from asyncio import AbstractEventLoop, Semaphore, wait_for, Future
 from pathlib import Path
 from typing import Iterator, Optional
 
@@ -9,16 +9,18 @@ import cchardet
 
 class AdvanceSemaphore(Semaphore):
     _loop: AbstractEventLoop
+    _finshed_future: Optional[Future]
 
     def __init__(self, value: int) -> None:
         super().__init__(value=value)
         self._initial_value = value
-        self._finshed_future = self._loop.create_future()
+        self._finshed_future = None
 
     def _check_value(self) -> None:
         if (
             (self._value >= self._initial_value)
             and (not self._waiters)
+            and (self._finshed_future is not None)
             and (not self._finshed_future.done())
         ):
             self._finshed_future.set_result(None)
@@ -28,6 +30,7 @@ class AdvanceSemaphore(Semaphore):
         self._check_value()
 
     async def wait_all_finish(self, timeout: float = None):
+        self._finshed_future = self._loop.create_future()
         self._check_value()
         await wait_for(self._finshed_future, timeout)
 
