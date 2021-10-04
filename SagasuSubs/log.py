b6297821e1e217404a7b9c676649951c1b2f8b0f
Mix <32300164+mnixry@users.noreply.github.com>
2021-10-04 15:46:38 +0800

    :loud_sound: Add extrenal logging handler

--- SagasuSubs/log.py
+++ SagasuSubs/log.py
@@ -1,7 +1,30 @@
+import logging
+from typing import Union
 from loguru import logger as _logger
 from tqdm import tqdm
 
 _logger.remove()
 _logger.add(lambda text: tqdm.write(text, end=""), colorize=True, enqueue=True)
+
+
+class LoguruHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord):
+        # Get corresponding Loguru level if it exists
+        level: Union[int, str]
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back  # type: ignore
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
 
 logger = _logger
